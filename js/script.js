361e70f06e3e36b7e01f5928a9d6d3d3d4fe4f1a
Gheorghe <madangheorghe.30@gmail.com>
2025-01-13 14:56:54 +0100

    fine esercizio js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@
 
 // Selezioniamo gli elementi di input dal html 
 const pathField = document.getElementById('path-field')
-const titleField = document.getElementById('title-field')
+const ageField = document.getElementById('age-field')
 const form = document.querySelector('form')
 
 // selezioniamo l'elemento di output 
 const outputPath = document.getElementById('output-path')
-const outputTitle = document.getElementById('output-title')
+const outputAge = document.getElementById('output-age')
+const outputResult = document.getElementById('result')
 
 form.addEventListener('submit', (event) => {
     // blocco l'invio del form
@@ -20,47 +21,45 @@ form.addEventListener('submit', (event) => {
     // vedo se funziona
     console.log("provo ad inviare il form");
 
-    // prendo i selettori di output e inserisco i valori ricevuti dal input
-    outputPath.innerHTML = pathField.value;
-    outputTitle.innerHTML = titleField.value;
+    // // prendo i selettori di output e inserisco i valori ricevuti dal input
+    // outputPath.innerHTML = pathField.value;
+    // outputAge.innerHTML = ageField.value;
+
+    const km = parseInt(pathField.value);
+    const age = parseInt(ageField.value);
+
+    // prezzi e sconto da eseguire
+    const costoPerkm = 0.20;
+    const sconto20 = 0.20;
+    const sconto40 = 0.40;
+
+    // il PC  calcola quanto spenderà in base ai km che l'utente farà
+    let costoTotale = km * costoPerkm;
+    console.log(costoTotale.toFixed(2))
+    // INPUT dell'età del cliente
+    // let eta = parseInt(prompt("Inserisci i l'età: "))
+        // SE ha meno di 18 anni
+        if (age < 18) {
+            // allora avrà uno sconto del 20%
+            let scontoMinorenne = costoTotale * sconto20
+            let prezzoScontatoMinore = costoTotale - scontoMinorenne
+            outputResult.innerHTML = prezzoScontatoMinore.toFixed(2);
+        }
+        // ALTRIMENTI SE ha più di 65 anni  
+        else if (age >= 65) {
+            // allora avrà uno sconto del 40%
+            let scontoSenior = costoTotale * sconto40
+            let prezzoScontatoSenior = costoTotale - scontoSenior
+            outputResult.innerHTML = prezzoScontatoSenior.toFixed(2);
+            }
+        // ALTRIMENTI paga il prezzo intero
+        else {
+            outputResult.innerHTML = costoTotale.toFixed(2)
+        }
 }); 
 
 
 
 
 
-
-// const costoPerkm = 0.20;
-// const sconto20 = 0.20;
-// const sconto40 = 0.40;
-// // INPUT dei km che il cliente vuole percorrere
-// let km = parseInt(prompt("Inserisci i kilometri da percorrere:"))
-// // Validazione, controllo se l'utente ha inserito corettamente i dati 
-// if (isNaN(km) || km <= 10) {
-//     alert("Per favore, inserisci un numero superiore a 10")
-// } else {
-//     // il PC  calcola quanto spenderà in base ai km che l'utente farà
-//     let costoTotale = km * costoPerkm
-//     console.log(costoTotale.toFixed(2))
-//     // INPUT dell'età del cliente
-//     let eta = parseInt(prompt("Inserisci i l'età: "))
-//         // SE ha meno di 18 anni
-//         if (eta < 18) {
-//             // allora avrà uno sconto del 20%
-//             let scontoMinorenne = costoTotale * sconto20
-//             let prezzoScontatoMinore = costoTotale - scontoMinorenne
-//             console.log(prezzoScontatoMinore.toFixed(2))
-//         }
-//         // ALTRIMENTI SE ha più di 65 anni  
-//         else if (eta >= 65) {
-//             // allora avrà uno sconto del 40%
-//             let scontoSenior = costoTotale * sconto40
-//             let prezzoScontatoSenior = costoTotale - scontoSenior
-//             console.log(prezzoScontatoSenior.toFixed(2))
-//             }
-//         // ALTRIMENTI paga il prezzo intero
-//         else {
-//             console.log(costoTotale.toFixed(2))
-//         }
-// }
 
